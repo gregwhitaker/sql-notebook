568812f31394181a983eb18aaaaa9d2c89c5912c
gregwhitaker <gwhitake@gmail.com>
2019-11-24 12:53:10 -0800

    Doing work

--- pg-histogram/example.sql
+++ pg-histogram/example.sql
@@ -17,11 +17,40 @@ GROUP BY bucket
 ORDER BY bucket;
 
 -- Histogram of the distribution of employee salaries across the company with bucket ranges shown (10 buckets)
-SELECT width_bucket(salary, 50000, 250000, 10) as bucket,
-       int4range(CAST(MIN(salary) AS INTEGER), CAST(MAX(salary) AS INTEGER)) as range,
+SELECT width_bucket(salary, 50000, 250000, 10) AS bucket,
+       int4range(cast(min(salary) AS INTEGER), cast(max(salary) AS INTEGER)) AS range,
        count(*)
 FROM employees
 GROUP BY bucket
 ORDER BY bucket;
 
 -- Draw a histogram in the console
+-- 1	[50422,69902)	91	■■■■■■■■■■■■■■■■■■■■■■■
+-- 2	[70579,89944)	99	■■■■■■■■■■■■■■■■■■■■■■■■■
+-- 3	[90502,109966)	106	■■■■■■■■■■■■■■■■■■■■■■■■■■
+-- 4	[110326,129867)	86	■■■■■■■■■■■■■■■■■■■■■
+-- 5	[130338,149944)	106	■■■■■■■■■■■■■■■■■■■■■■■■■■
+-- 6	[150162,169872)	105	■■■■■■■■■■■■■■■■■■■■■■■■■■
+-- 7	[170025,189920)	102	■■■■■■■■■■■■■■■■■■■■■■■■■
+-- 8	[190361,209859)	84	■■■■■■■■■■■■■■■■■■■■■
+-- 9	[210776,229683)	100	■■■■■■■■■■■■■■■■■■■■■■■■■
+-- 10	[230091,249963)	121	■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+
+WITH salary_stats AS (
+    SELECT min(salary) as min,
+           max(salary) as max
+    FROM employees
+),
+histogram AS (
+    SELECT width_bucket(salary, 50000, 250000, 10) AS bucket,
+           int4range(cast(min(salary) AS INTEGER), cast(max(salary) AS INTEGER)) AS range,
+           count(*) AS freq
+    FROM employees, salary_stats
+    GROUP BY bucket
+    ORDER BY bucket
+)
+SELECT bucket,
+       range,
+       freq,
+       repeat('■', (freq::float / max(freq) over() * 30)::int) AS bar
+from histogram;

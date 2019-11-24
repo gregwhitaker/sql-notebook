438d422f566c4fed707b4005dfd3d786ba06ba51
gregwhitaker <gwhitake@gmail.com>
2019-11-24 12:45:00 -0800

    Doing work

--- pg-histogram/example.sql
+++ pg-histogram/example.sql
@@ -1,25 +1,22 @@
 -- Postgres Histogram Examples
 
---
--- Histogram of the distribution of employee salaries across the company
---
-
--- Determine what kind of range we are working with:
+-- Before creating histograms you need to do some queries to figure out
+-- the range of data you are working with. In this case we need to figure
+-- out the range of salaries across the company:
 SELECT min(salary) AS min_salary,
        max(salary) AS max_salary
 FROM employees;
--- Result is a min_salary of $50,422 and a max_salary of $249,963
 
--- Determine a number of buckets to create based on the range (10 buckets in this example):
+-- This query tells us that we have a min_salary of $50,422 and a max_salary of $249,963
+
+-- Histogram of the distribution of employee salaries across the company (10 buckets)
 SELECT width_bucket(salary, 50000, 250000, 10) as bucket,
        count(*)
 FROM employees
 GROUP BY bucket
 ORDER BY bucket;
 
---
--- Histogram of the distribution of employee salaries across the company with bucket ranges shown:
---
+-- Histogram of the distribution of employee salaries across the company with bucket ranges shown (10 buckets)
 SELECT width_bucket(salary, 50000, 250000, 10) as bucket,
        int4range(CAST(MIN(salary) AS INTEGER), CAST(MAX(salary) AS INTEGER), '[]') as range,
        count(*)

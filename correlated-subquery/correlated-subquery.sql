5efac4666f12f58cf788da26adec4ab8cdf1918e
gregwhitaker <gwhitake@gmail.com>
2019-11-24 12:06:06 -0800

    Doing work

--- correlated-subquery/correlated-subquery.sql
+++ correlated-subquery/correlated-subquery.sql
@@ -8,12 +8,13 @@ WHERE salary > (SELECT AVG(salary)
                 WHERE department = e.department);
 
 -- Find all employees that make more than the average salary in their department
--- and show the department average
+-- and show the department average with difference
 SELECT e.id,
        e.last_name,
        e.salary,
        e.department,
-       (SELECT AVG(salary) FROM employees WHERE department = e.department) AS dept_avg_salary
+       (SELECT ROUND(AVG(salary)) FROM employees WHERE department = e.department) AS dept_avg_salary,
+       e.salary - (SELECT ROUND(AVG(salary)) FROM employees WHERE department = e.department) AS salary_diff
 FROM employees e
 WHERE salary > (SELECT AVG(salary)
                 FROM employees
